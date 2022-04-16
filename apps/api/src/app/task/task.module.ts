116377aa066358ab4cdcc57b0a3c91cd5e161a3a
JenHsuan <ofalpha@gmail.com>
2022-04-16 15:51:09 +0800

    Add SSL parmeter

--- apps/api/src/app/task/task.module.ts
+++ apps/api/src/app/task/task.module.ts
@@ -4,16 +4,23 @@ import { Task } from '../database/entity/task/task.entity';
 import { TaskController } from './controller/task.controller';
 import { TaskService } from './task/task.service';
 import { getConnectionOptions } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
     imports: [
-      TypeOrmModule.forRootAsync({
-        useFactory: async () =>
-          Object.assign(await getConnectionOptions(), {
-            autoLoadEntities: true,
-          }),
-      }),
-      TypeOrmModule.forFeature([Task]),
+        TypeOrmModule.forRootAsync({
+            useFactory: async () =>
+                Object.assign(await getConnectionOptions(), {
+                    autoLoadEntities: true,
+                    ssl: true,
+                    extra: {
+                        ssl: {
+                            rejectUnauthorized: false
+                        }
+                    }
+                }),
+        }),
+        TypeOrmModule.forFeature([Task]),
     ],
     controllers: [TaskController],
     providers: [TaskService],
