4e3ef1d2976d24465ff5e16f014393c41378f32b
JenHsuan <ofalpha@gmail.com>
2022-05-02 21:19:47 +0800

    Add angular element

--- apps/nx-nest-postgre-manager/src/app/app.module.ts
+++ apps/nx-nest-postgre-manager/src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AccountModule } from './account/account.module';
 
+import { createCustomElement } from '@angular/elements';
+import { AccountComponent } from './account/account.component';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, AccountModule],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents: [AccountComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {
+
+  }
+  ngDoBootstrap() {
+    const accountElement = createCustomElement(AccountComponent, { injector: this.injector });
+    customElements.define('dl-account', accountElement);
+  }
+}
