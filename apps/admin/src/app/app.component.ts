1727a9bb7fd7e7373dc063584c0c159d35323026
JenHsuan <ofalpha@gmail.com>
2022-05-21 08:43:03 +0800

    Add a new application

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-nest-postgre-manager-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'admin';
+}
