1679077d0261bdb1cdbe5992bfeabef8de749b72
JenHsuan <ofalpha@gmail.com>
2022-11-07 21:47:26 +0800

    Add the auth service

--- libs/auth/src/service/auth.service.ts
+++ libs/auth/src/service/auth.service.ts
@@ -1,30 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { catchError, map, Observable, of } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, of } from 'rxjs';
 import { Env, ENV_TOKEN } from '@nx-nest-postgre-manager/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  accessTokenSubject: BehaviorSubject<string>;
   private BASE_URL: string;
   constructor(
     private http: HttpClient,
     @Inject(ENV_TOKEN) private env: Env
   ) {
     this.BASE_URL = `${this.env.baseApiPrefix}/auth`;
+    this.accessTokenSubject = new BehaviorSubject('');
   }
 
   FetchToken(username: string, password: string): Observable<string> {
     return this.http.post<any>(`${this.BASE_URL}/login/`, { username, password }).pipe(
       map(res => {
-        if (!res.access_token) {
-          return of('')
-        }
-        return res.access_token
+        this.accessTokenSubject.next(res.access_token);
+        return res.access_token ? res.access_token : of('');
       }),
       catchError(err => {
         console.log(err);
+        this.accessTokenSubject.next('');
         return of(err);
       })
     );
